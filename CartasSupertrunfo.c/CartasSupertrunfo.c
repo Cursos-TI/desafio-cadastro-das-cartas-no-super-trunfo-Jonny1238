d5bec239dd9fe658b5f38ba612d1fc4159b05b28
João <joaoguimaraedmotta@gmail.com>
2025-04-10 02:33:03 +0000

    criação das cartas super trunfo

--- CartasSupertrunfo.c/CartasSupertrunfo.c
+++ CartasSupertrunfo.c/CartasSupertrunfo.c
@@ -1,7 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main(){
-    printf("inicio");
+    //("carta super trunfo brasil")
+    int População1 = 211000000;
+    float  Area1 = 8.510000;
+    float Pib1 = 11.700000000000;
+    int Pontosturisticos1 = 70;
+    char Pais1[20] = "brasil";
+
+    //("carta super trunfo EUA")
+    int População2 = 340.000000;
+    float  Area2 = 9.867000;
+    float Pib2 = 27.000000000000;
+    int Pontosturisticos2 = 230;
+    char Pais2 [30] = "United States of America";
+
+    //
+
+
 
 
 
